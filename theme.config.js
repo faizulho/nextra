e55221479653f4f441004b8aa70dbca9dcb7c570
Muhammad Faizullah Syarizzat <muhdfaizul@mail.ru>
2021-06-28 17:42:46 +0800

    Update theme.config.js

--- theme.config.js
+++ theme.config.js
@@ -12,7 +12,7 @@ export default {
   head: (
     <>
       <meta name="msapplication-TileColor" content="#0285cc" />
-      <meta name="theme-color" content="#0285cc" />
+      <meta name="theme-color" content="#ffffff" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta httpEquiv="Content-Language" content="en" />
       <meta name="description" content="ImMuhdFaizul: Legal Policies" />
