9ed34e95e1efea8a18e572a8d20e89efc72cb2dc
Muhammad Faizullah Syarizzat <muhdfaizul@mail.ru>
2021-06-28 17:37:15 +0800

    Update theme.config.js

--- theme.config.js
+++ theme.config.js
@@ -1,18 +1,18 @@
 export default {
-  repository: 'https://github.com/shuding/nextra',
-  titleSuffix: ' – Nextra',
+  repository: 'https://github.com/faizulho/nextra',
+  titleSuffix: ' – ImMuhdFaizul',
   logo: (
     <>
       <span className="mr-2 font-extrabold hidden md:inline">ImMuhdFaizul</span>
-      <span className="text-gray-600 font-normal hidden md:inline">
+      <span className="text-blue-600 font-normal hidden md:inline">
         Legal Policies
       </span>
     </>
   ),
   head: (
     <>
-      <meta name="msapplication-TileColor" content="#ffffff" />
-      <meta name="theme-color" content="#ffffff" />
+      <meta name="msapplication-TileColor" content="#0285cc" />
+      <meta name="theme-color" content="#0285cc" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta httpEquiv="Content-Language" content="en" />
       <meta name="description" content="ImMuhdFaizul: Legal Policies" />
